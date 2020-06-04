179ea21115f3ad1f47309c072325d299cf7f4fe2
junping.wu <9700616@qq.com>
2020-06-05 00:58:13 +0800

    ❤

--- src/gulpCore/uniRequire.js
+++ src/gulpCore/uniRequire.js
@@ -3,10 +3,10 @@ const {
     regExpWxResources,
     regExpUniRequire,
 } = require('./preset')
-const {tryAgain, getLevelPath, getLevel, writeLastLine} = require('./utils')
+const {getLevelPath, getLevel} = require('./utils')
 
 exports.uniRequireWxResource = function uniRequireWxResource () {
-    return $.replace(regExpUniRequire, function (match, p1, offset, string) {
+    return $.replace(regExpUniRequire, function (match, p1) {
         const pathLevel = getLevel(this.file.relative)
         console.log(`\n编译${match}-->require(${p1.replace(regExpWxResources, getLevelPath(pathLevel))})`)
         return `require(${p1.replace(regExpWxResources, getLevelPath(pathLevel))})`
